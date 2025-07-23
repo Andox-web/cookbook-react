78e6f14679c21f3f368d56b4fc1a4343f7f88a45
Tovoarinaina Ando <154896546+Andox-web@users.noreply.github.com>
2025-07-23 19:08:09 +0300

    add and edit

--- src/features/recipe/types/RecipeTypes.ts
+++ src/features/recipe/types/RecipeTypes.ts
@@ -3,7 +3,7 @@ export type RecipeCategory = 'Entrée' | 'Plat' | 'Dessert' | 'Boisson' | 'Autre
 export interface Recipe {
   id: string;
   title: string;
-  image: string; // image encodée en base64 (ex: "data:image/png;base64,...")
+  image: string;
   duration: number; // en minutes
   category: RecipeCategory;
   ingredients: string[];
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import AppLayout from '../layouts/AppLayout';
-import { Home } from '../features/recipe/components/Home/Home';
-import { Favorites } from '../features/recipe/components/Favorites/Favorites';
+import { Home } from '../features/recipe/pages/Home/Home';
+import { Favorites } from '../features/recipe/pages/Favorites/Favorites';
+import { RecipeDetail } from '../features/recipe/pages/RecipeDetail/RecipeDetail';
+import { AddRecipe } from '../features/recipe/pages/add/AddRecipe';
 
 const AppRoutes = () => (
   <Routes>
@@ -13,12 +15,27 @@ const AppRoutes = () => (
         </AppLayout>
       }
     />
-    <Route path="/add" element={<AppLayout>ATO LAY PAGE /add </AppLayout>} />
+    <Route
+      path="/add"
+      element={
+        <AppLayout>
+          <AddRecipe />
+        </AppLayout>
+      }
+    />
     <Route
       path="/favorites"
       element={
         <AppLayout>
           <Favorites />
+        </AppLayout>
+      }
+    />
+    <Route
+      path="/recipes/:id"
+      element={
+        <AppLayout>
+          <RecipeDetail />
         </AppLayout>
       }
     />
